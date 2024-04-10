c391484f3bcfd63a3d44534a053ec8e8931a2584
Fatima Badaoui <fatimina615@gmail.com>
2024-04-10 09:50:42 +0200

    add descriptve comments

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,29 +1,43 @@
 import asyncHandler from "../config/asyncHandler.js";
 import ItemModel from "../models/ItemModel.js";
 
-// CREATE the items
-
+/* 
+@desc    Create a new item
+@route   POST /api/items
+@access  Public
+*/
 const createItem = asyncHandler(async (req, res) => {
   const item = await ItemModel.create(req.body);
   res.status(201).json(item);
 });
 
-// READ the items
-
+/* 
+@desc    Get all items
+@route   GET /api/items
+@access  Public
+*/
 const getAllItems = asyncHandler(async (req, res) => {
   const items = await ItemModel.find();
   res.status(200).json(items);
 });
 
-// READ each item
 
+/* 
+@desc    Get an item by ID
+@route   GET /api/items/:id
+@access  Public
+*/
 const getItemById = asyncHandler(async (req, res) => {
   const item = await ItemModel.findById(req.params.id);
   res.status(200).json(item);
 });
 
-// UPDATE the items
 
+/* 
+@desc    Update an item
+@route   PATCH /api/items/:id
+@access  Public
+*/
 const updateItem = asyncHandler(async (req, res) => {
   const itemUpdated = await ItemModel.findByIdAndUpdate(
     req.params.id,
@@ -37,15 +51,23 @@ const updateItem = asyncHandler(async (req, res) => {
   res.status(200).json(itemUpdated);
 });
 
-// DELETE one item
 
+/* 
+@desc    Delete an item
+@route   DELETE /api/items/:id
+@access  Public
+*/
 const deleteItem = asyncHandler(async (req, res) => {
   const itemDeleted = await ItemModel.findByIdAndDelete(req.params.id);
   res.status(200).json(itemDeleted);
 });
 
-// DELETE all items
 
+/* 
+@desc    Delete all items
+@route   DELETE /api/items
+@access  Public
+*/
 const deleteAllItems = asyncHandler(async (req, res) => {
   await ItemModel.deleteMany();
   res.status(200).json({ message: "All items are deleted." });
